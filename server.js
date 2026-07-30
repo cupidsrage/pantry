6a6944f7ea6c7d3014a9937cbe3216419873e67d
cupidsrage <cupidsrage@gmail.com>
2026-07-29 21:38:31 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -677,6 +677,6 @@ app.delete("/api/recipes/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.get("/api/version", (_, res) => res.json({ version: "pantry-2026-07-29z" }));
+app.get("/api/version", (_, res) => res.json({ version: "pantry-2026-07-30a" }));
 
-app.listen(PORT, () => console.log(`Pantry running on ${PORT} [pantry-2026-07-29z]`));
+app.listen(PORT, () => console.log(`Pantry running on ${PORT} [pantry-2026-07-30a]`));
